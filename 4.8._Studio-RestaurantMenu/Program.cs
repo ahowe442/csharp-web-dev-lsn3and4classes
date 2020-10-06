9dcb78cd689cc99745b18949760ba6a4809f90d1
Abby Howe <howe.abigail@gmail.com>
2020-10-06 10:39:43 -0500

    RestaurantMenu Initial Commit

---
+++ 4.8._Studio-RestaurantMenu/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _4._8._Studio_RestaurantMenu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
